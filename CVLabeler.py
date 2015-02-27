728a4941201a027c035cee8da1277b2d015ca59c
Jay Hack <jhack@stanford.edu>
2015-02-27 05:49:01 -0800

    now correctly getting clicks. moving on to storing

--- CVLabeler.py
+++ CVLabeler.py
@@ -1,21 +1,26 @@
 import cv
 import cv2
+from operator import itemgetter
 
 class CVLabeler(object):
 
 	window_name = 'DISPLAY'
 	# window_size = (640, 480)
 
-	def __init__(self, get_objects, draw_object):
+	def __init__(self, get_objects, draw_object, get_distance, update_object):
 		"""
 			get_objects: image -> objects  
 			draw_object: (image, objects) -> drawn image
+			get_distance: (object, x, y) -> distance from (x, y)
+			update_object: (event, label) -> new label
 		"""
 		self.labels = None
 		self.objs = None
 
 		self.get_objects = get_objects
 		self.draw_object = draw_object
+		self.get_distance = get_distance
+		self.update_object = update_object
 
 		self.create_display()
 
@@ -28,7 +33,6 @@ class CVLabeler(object):
 	def create_display(self):
 		"""creates window and attaches callback"""
 		cv2.namedWindow(self.window_name)
-		# cv2.resizeWindow(self.window_name, self.window_size[0], self.window_size[1])
 		cv2.setMouseCallback(self.window_name, self.mouse_callback)
 
 
@@ -37,22 +41,34 @@ class CVLabeler(object):
 		cv2.destroyWindow(self.window_name)
 
 
-	def get_closest_obj(self, x, y):
+	def reset_objs_labels(self, image):
+		"""resets self.objs and self.labels from image"""
+		self.objs = self.get_objects(image)
+		self.labels = [None for obj in self.objs]
+
+
+	def get_closest_obj(self, objs, x, y):
 		"""returns closest x,y"""
-		raise NotImplementedError()
+		distances = [self.get_distance(obj, x, y) for obj in objs]
+		ix, dist = min(enumerate(distances), key=itemgetter(1))
+		return ix, objs[ix]
 
 
 	def mouse_callback(self, event, x, y, flags, param):
 		"""handles mouse input"""
 		if event == cv2.EVENT_LBUTTONDOWN:
 			print "BUTTON CLICK: (%d, %d)" % (x, y)
+			ix, obj = self.get_closest_obj(self.objs, x, y)
+			print 'before: ', self.labels
+			self.labels[ix] = self.update_object(event, self.labels[ix])
+			print 'after: ', self.labels
 
 
-	def annotate_image(self, image, objs):
+	def annotate_image(self, image, objs, labels):
 		"""updates image based on current labels""" 
 		disp_img = image.copy()
-		for obj in objs:
-			disp_img = self.draw_object(disp_img, obj)
+		for obj, label in zip(objs, labels):
+			disp_img = self.draw_object(disp_img, obj, label)
 		return disp_img
 
 
@@ -65,16 +81,18 @@ class CVLabeler(object):
 			return map(self.label, image)
 
 		#=====[ Step 2: get objects	]=====
-		objs = self.get_objects(image)
+		self.reset_objs_labels(image)
 
 		#=====[ Step 3: loop while annotating 	]=====
 		while True:
 
-			disp_img = self.annotate_image(image, objs)
+			disp_img = self.annotate_image(image, self.objs, self.labels)
 			cv2.imshow(self.window_name, disp_img)
 
-			if cv2.waitKey(20) & 0xFF == 27:
+			key = cv2.waitKey(20)
+			if key & 0xFF == 27:
 				break
+
 
 
 
--- demo.py
+++ demo.py
@@ -6,20 +6,37 @@ if __name__ == '__main__':
 
 	def get_objects(image, npoints=4):
 		"""returns 'npoints' random (x,y) coordinates"""
-		xtop, ytop = image.shape
+		xtop, ytop, nchannels = image.shape
 		xs = np.random.randint(0, xtop, (npoints,1))
 		ys = np.random.randint(0, ytop, (npoints,1))
 		return [(int(xs[i]), int(ys[i])) for i in range(npoints)]
 
-	def draw_object(image, obj):
+
+	def draw_object(image, obj, label):
 		"""draws a small circle around obj"""
 		disp_img = image.copy()
-		cv2.circle(disp_img, obj, 2, color=255, thickness=1)
+		colors = {
+					None:(0, 0, 255),
+					False:(0, 0, 255),
+					True:(0, 255, 0)
+				}
+		cv2.circle(disp_img, obj, 5, color=colors[label], thickness=3)
 		return disp_img
 
-	images = [np.zeros((400, 400)).astype(np.uint8) for i in range(3)]
 
-	labeler = CVLabeler(get_objects, draw_object)
+	def get_distance(obj, x, y):
+		"""returns distance from object to x, y"""
+		return (obj[0]-x)**2 + (obj[1]-y)**2
+
+
+	def update_object(event, label):
+		"""flips label (works with None as well)"""
+		return not label
+
+
+	images = [np.zeros((400, 400, 3)).astype(np.uint8) for i in range(3)]
+
+	labeler = CVLabeler(get_objects, draw_object, get_distance, update_object)
 	for image in images:
 		labeler.label(image)
 
